8b764e690a6c70808bef5ccbcec3abecbfbf1762
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-08-20 14:50:17 -0300

    Avancei na Okean

--- Aulas Curso em Vídeo/Desafio 030 - Aula 10 - Condições Simples e Composta.py
+++ Aulas Curso em Vídeo/Desafio 030 - Aula 10 - Condições Simples e Composta.py
@@ -1,10 +1,5 @@
 numero = int(input('Digite seu número: '))
-if (numero//2) % 2 == 0:
-    print('Seu número é par')
+if (numero%2)==0:
+    print('Seu Número é Par')
 else:
-    print('Seu número é impar')
-
-    Está errado preciso corrigir
-
-
-    https://www.youtube.com/watch?v=K10u3XIf1-Q&t=1238s&ab_channel=CursoemV%C3%ADdeo PAREI NO MINUTO 29
+    print('Seu Número é Impar')
---
+++ Aulas Curso em Vídeo/Desafio 031 - Aula 10 - Condições Simples e Composta.py
@@ -0,0 +1,3 @@
+
+
+ https://www.youtube.com/watch?v=K10u3XIf1-Q&t=1238s&ab_channel=CursoemV%C3%ADdeo PAREI NO MINUTO 29
