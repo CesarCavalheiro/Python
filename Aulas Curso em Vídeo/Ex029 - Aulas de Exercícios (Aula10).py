312b3e48d4276b0bbb133075f10ee67bf0bcbfda
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-08-22 20:01:15 -0300

    Exercícios feitos

--- Aulas Curso em Vídeo/Ex029 - Aulas de Exercícios (Aula10).py
+++ Aulas Curso em Vídeo/Ex029 - Aulas de Exercícios (Aula10).py
@@ -2,6 +2,7 @@ velocidade = float(input('Qual é a velocidade atual do carro?\033[1;32m '))
 if(velocidade <= 80):
     print('Dirija com segurança!')
 else:
-    print(f'\033[1;31mMULTADO! Você execedeu o limite de velocidade em {velocidade-50:.2f}Km/h.\033[m \nSua multa será de R$ {(velocidade-50)*7:.2f}! Dirija com segurança e RESPEITE OS LIMITES DE VELOCIDADE.')
+    print(f'\033[1;31mMULTADO! Você execedeu o limite de velocidade em {velocidade-80:.2f}Km/h.\033[m \nSua multa será de R$ {(velocidade-8100)*7:.2f}! Dirija com segurança e RESPEITE OS LIMITES DE VELOCIDADE.')
 print('\033[33mTenha um bom dia!!!\033[m')
 
+100
