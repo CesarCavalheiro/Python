d724d1bec1bc2fdc9d243f395659e04671a0d1ea
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-09-14 18:36:10 -0300

    Exercícios feitos

--- Aulas Curso em Vídeo/Desafio 051 - Aula 13 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 051 - Aula 13 - Estruturas de Repetição.py
@@ -1,11 +1,13 @@
 print('Calculadora de P.A')
-termo = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão desejada: '))
-p_a = termo + razao
-for p_a in range(1,11):
-    print(p_a)
+termo = int(input('Digite o primeiro termo: '))   
+razao = int(input('Digite a razão: '))
+razao = for razao in range(0,11):
+    print(razao)
+# p_a = termo + razao
+# # for p_a in range(1,11):
+# print(p_a)
 
-    EXERCICIO NÃO FINALIZADO
+    # EXERCICIO NÃO FINALIZADO
 
 
-    https://www.youtube.com/watch?v=cL4YDtFnCt4&ab_channel=CursoemV%C3%ADdeo parei no minuto 29:57
+    # https://www.youtube.com/watch?v=cL4YDtFnCt4&ab_channel=CursoemV%C3%ADdeo parei no minuto 29:57
