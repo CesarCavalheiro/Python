c1979d0c3689d3fe2c3e8aa41b2b97a1446aca60
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-09-24 14:02:38 -0300

    Avanço nos exercícios

--- Aulas Curso em Vídeo/Desafio 054 - Aula 13 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 054 - Aula 13 - Estruturas de Repetição.py
@@ -1,6 +1,10 @@
-dt_nasc = int(input('Digite a Data de Nascimento de 7 Pessoas: '))
-for dt_nasc in range(1,8):
-        print(dt_nasc)
+for c in range (1,8):
+        dt_nasc = int(input(f'Digite a Data de Nascimento da {c}ª Pessoa: '))
+        if dt_nasc <= 18:
+                print('Você tem menos que 18 anos, você ainda é menor de idade.')
+        else:
+                print('Você tem 18 ou +, você já é maior de idade.')
+                
    
    
    
