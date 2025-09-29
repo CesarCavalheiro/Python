9623edcd3c4cc81c5e564e583817d8bff470252e
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-09-29 14:12:18 -0300

    Avanço de exercícios

--- Aulas Curso em Vídeo/Desafio 056 - Aula 13 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 056 - Aula 13 - Estruturas de Repetição.py
@@ -1,12 +1,15 @@
-print('Digite os nomes, idades e sexo de 4 pessoas.')
+print('Digite os nomes, idades e sexo de 4 pessoas.\n')
 
 nomes = []
 sexos = []
 idades = []
 
 for c in range(1,5):
-    informações = (f'Digite o nome, o sexo e a idade da {c}ª pessoa: ')
+    nome = input(f'Digite o nome da {c}ª pessoa: ')
+    sexo = input(f'Digite o sexo da {c}ª pessoa: ')
+    idade = int(input(f'Digite a idade da {c}ª pessoa: '))
     nomes.append(nome)
     sexos.append(sexo)
     idades.append(idade)
-print(f'Qual a idade média do grupo? {idade}')
+soma = sum(idades)/4
+print(f'\nQual a idade média do grupo? {soma:.0f} anos!')
