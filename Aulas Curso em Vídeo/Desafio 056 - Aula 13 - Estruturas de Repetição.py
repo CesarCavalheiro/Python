db547d823a80cc1d709769db6262cd6738c11c91
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-02 14:18:49 -0300

    Avanço nos exercícios

--- Aulas Curso em Vídeo/Desafio 056 - Aula 13 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 056 - Aula 13 - Estruturas de Repetição.py
@@ -16,8 +16,11 @@ mais_velho = max(idades)
 mais_novo = min(idades)
 indice_mais_velho = idades.index(mais_velho)
 indice_mais_novo = idades.index(mais_novo)
-indice_mulheres_abaixo20 = nomes.index(mais_novo)
 print(f'\nQual a idade média do grupo? {soma:.0f} anos!')
 print(f'A pessoa mais velha é {nomes[indice_mais_velho]}, com {mais_velho} anos.')
 print(f'A pessoa mais nova é {nomes[indice_mais_novo]} com {mais_novo} anos.')
-print(f'')
+mulheres_menos_de_20 = 0
+for i in range(len(sexos)):
+    if sexos[i].upper()== 'F' and idades[i] <20:
+        mulheres_menos_de_20 += 1
+print(f'A quantidade de mulheres com menos de 20 anos é: {mulheres_menos_de_20}')
