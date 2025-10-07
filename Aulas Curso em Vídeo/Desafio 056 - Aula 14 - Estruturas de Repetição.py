a758c0f35ad035df3b89f1999c3e7ec6e351113d
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-07 14:06:54 -0300

    Avanço nos exercícios

---
+++ Aulas Curso em Vídeo/Desafio 056 - Aula 14 - Estruturas de Repetição.py
@@ -0,0 +1,6 @@
+print('Digite seu sexo: \nAtenção só serão aceitos sexos M para masculino e F para feminino')
+while not == [M,F]:
+    print('Digite seu sexo: \nAtenção só serão aceitos sexos M para masculino e F para feminino')
+
+
+    https://www.youtube.com/watch?v=LH6OIn2lBaI
