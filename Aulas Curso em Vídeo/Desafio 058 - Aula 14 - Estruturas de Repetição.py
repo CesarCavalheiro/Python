a771d890156696bac5b24a56037b41a5810ef86f
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-13 14:10:52 -0300

    Avanço nos exercícios.

--- Aulas Curso em Vídeo/Desafio 058 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 058 - Aula 14 - Estruturas de Repetição.py
@@ -1,14 +1,13 @@
 from random import randint
+
+ntentativa = 0
 n = randint(0,10)
 tentativa = int(input('Digite um número de 0 a 10 e tente adivinhar em qual número o computador pensou: '))
 while n != tentativa:
-print(f'O computador escolheu {n}, você digitou {tentativa}. Você errou, tente novamente, digite um número diferente: ')
-if n == tentativa:
-    print(f'O computador escolheu {n}, você digitou {tentativa}. Você acertou, parabéns!')
-    # else:
-    #     (f'O computador escolheu {n}, você digitou {tentativa}. Você errou, tente novamente, digite um número diferente: ')
-
-
+    tentativa = int(input(f'Você digitou {tentativa}. Você errou, tente novamente, digite um número diferente: '))
+    ntentativa += 1
+print(f'O computador escolheu {n}, você digitou {tentativa}. Você precisou de {ntentativa} tentativas, mas acertou, parabéns!')
+    
 
 
 #https://www.youtube.com/watch?v=LH6OIn2lBaI
