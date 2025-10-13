384c1966b7c64259f74cf8647a48192c15718011
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-13 13:41:42 -0300

    Avanço nos exercícios

--- Aulas Curso em Vídeo/Desafio 057 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 057 - Aula 14 - Estruturas de Repetição.py
@@ -1,6 +1,11 @@
+s = ''
+while s not in ('M', 'F'):
+    s = str(input('Digite seu sexo. Utilize M para Masculino e F para Feminino: ')).upper().strip()
+    if s == 'M':
+        print('Você é homem')
+    if s == 'F':
+        print('Você é mulher')
+    else:
+        print('Comando inválido! Digite apenas M ou F.\n')
 
 
-
-
-
-https://www.youtube.com/watch?v=LH6OIn2lBaI
---
+++ Aulas Curso em Vídeo/Desafio 058 - Aula 14 - Estruturas de Repetição.py
@@ -0,0 +1,10 @@
+from random import randint
+n = randint(0,10)
+tentativa = int(input('Digite um número de 0 a 10 e tente adivinhar em qual número o computador pensou: '))
+while tentativa == n:
+    print('Você errou, tente novamente, digite um número diferente: ')
+
+
+
+
+#https://www.youtube.com/watch?v=LH6OIn2lBaI
