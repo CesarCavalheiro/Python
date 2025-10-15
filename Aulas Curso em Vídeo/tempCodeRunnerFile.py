a3cfe598e0a5b87b4c0ad088e4f1b1be28d70c70
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-15 14:05:03 -0300

    avanço nos estudos

--- Aulas Curso em Vídeo/tempCodeRunnerFile.py
+++ Aulas Curso em Vídeo/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
- # if opção == 1:
-    #     print(f'A soma dos seus números é: {somar}')
-    # if opção == 2:
-    #     print(f'A multiplicação dos seu numeros é: {multiplicar}')
-    # if opção == 3:
-    #     print(f'O maior entre seus números é: {maior}') 
-    # if opção == 4:
-    #     print('Digite novos números')
-    #     n1 = int(input('Digite o primeiro valor: '))
-    #     n2 = int(input('Digite o segundo valor: '))
-    # if opção == 5:
-    #     print('Você escolheu sair do programa.')
+#https://www.youtube.com/watch?v=LH6OIn2lBaI
