0bfad30991738075f44462d7b6448c67d54d39c1
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-15 13:49:08 -0300

    exercícios

--- Aulas Curso em Vídeo/Desafio 059 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 059 - Aula 14 - Estruturas de Repetição.py
@@ -1,20 +1,23 @@
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
-somar = n1 + n2
-multiplicar = n1 * n2
-maior = max(n1,n2)
-print('Escolha: \n1 somar \n2 multiplicar \n3 maior \n4 novos números \n5 sair do programa')
-opção = int(input('Digiteu sua opção: '))
+opção = 0
 while opção != 5:
-    if opção == 1:
-        print(f'A soma dos seus números é: {somar}')
-    if opção == 2:
-        print(f'A multiplicação dos seu numeros é: {multiplicar}')
-    if opção == 3:
-        print(f'O maior entre seus números é: {maior}') 
-    if opção == 4:
-        print('Digite novos números')
-        n1 = int(input('Digite o primeiro valor: '))
-        n2 = int(input('Digite o segundo valor: '))
-    if opção == 5:
-        print('Você escolheu sair do programa.')
+    print('\Escolha:')
+    print('\n1 Somar \n2 Multiplicar \n3 Mostrar o Maior \n4 Digitar novos numeros \n5 Sair do programa.')
+
+
+
+
+
+    # if opção == 1:
+    #     print(f'A soma dos seus números é: {somar}')
+    # if opção == 2:
+    #     print(f'A multiplicação dos seu numeros é: {multiplicar}')
+    # if opção == 3:
+    #     print(f'O maior entre seus números é: {maior}') 
+    # if opção == 4:
+    #     print('Digite novos números')
+    #     n1 = int(input('Digite o primeiro valor: '))
+    #     n2 = int(input('Digite o segundo valor: '))
+    # if opção == 5:
+    #     print('Você escolheu sair do programa.')
