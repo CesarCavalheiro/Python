4ea912e92c77a36aec430ec453aade8c1250e785
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-16 14:04:59 -0300

    avanço nos exercícios

---
+++ Aulas Curso em Vídeo/Desafio 060 - Aula 14 - Estruturas de Repetição.py
@@ -0,0 +1,19 @@
+# print('Calculadora de fatorial')
+# n = int(input('\nDigite seu número '))
+# fatorial = 1
+# for c in range(1,n+1):
+#     fatorial *= c
+#     print(fatorial)
+# print(f'O fatorial de {n} é {fatorial}')
+
+# Acima vimos como fazer com FOR, agora vamos tentar com WHILE.
+
+print('Calculadora de fatorial')
+n = int(input('\nDigite seu número '))
+fatorial = 1
+while fatorial != n:
+    fatorial *= n
+    print(f'O fatorial de {n} é {fatorial}')
+
+
+    https://www.youtube.com/watch?v=LH6OIn2lBaI&t=1818s
