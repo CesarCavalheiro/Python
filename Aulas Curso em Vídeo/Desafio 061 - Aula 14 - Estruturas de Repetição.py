2478cfc3c9614afe0128d2bf46f98836739ff29f
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-22 13:34:58 -0300

    Avanço nos exercícios

--- Aulas Curso em Vídeo/Desafio 061 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 061 - Aula 14 - Estruturas de Repetição.py
@@ -1,15 +1,13 @@
 print('Calculadora de P.A')
-termo = int(input('\nDigite o primeiro termo: '))   
+termo = int(input('\nDigite o termo: '))   
 razao = int(input('\nDigite a razão: '))
-n = razao
+nt = int(input('\nVocê gostaria de mostrar quantos termos? '))
 contador = 1
-while contador < 11:
-    pa = termo + razao
-    razao = razao + n
+print(f'Seu termo é: {termo}, sua razão é: {razao}, a quantidade de termos a ser mostrados é: {nt}, sua P.A. ficará assim: ')
+while contador < nt+1:
+    pa = termo + razao * contador
     contador += 1
     print(pa)
-
-
 
 
 
