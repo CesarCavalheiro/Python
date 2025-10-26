667e6045830c9ce2b3399643d99f8bd106dbf295
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-10-26 15:29:16 -0300

    Update Desafio 063 - Aula 14 - Estruturas de Repetição.py

--- Aulas Curso em Vídeo/Desafio 063 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 063 - Aula 14 - Estruturas de Repetição.py
@@ -1,13 +1,13 @@
 print('Calculadora de Fibonacci')
 
 fibo = int(input('\nDigite a quantidade de número na sequência de Fibonacci que você quer ver: '))
-n1 = 0
-n2 = 1
-contador = 0
+n1 , n2 = 0, 1
+contador = 2
+print(n1)
+print(n2)
 while fibo > contador:
-    sequencia1 = n1 + n2
-    sequencia2 = (sequencia1 + 1) + sequencia1
+    sequencia = n1 + n2
+    n1 = n2
+    n2 = sequencia
     contador += 1
-    # print(n1)
-    # print(n2)
     print(sequencia)
