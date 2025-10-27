e92225432378fdd142a5a5f22fcad84d1f03e6e9
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-27 14:48:38 -0300

    Update Desafio 064 - Aula 14 - Estruturas de Repetição.py

--- Aulas Curso em Vídeo/Desafio 064 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 064 - Aula 14 - Estruturas de Repetição.py
@@ -3,6 +3,7 @@ n = 0
 contador = 0
 while n != 999:
     n = int(input('\nDigite o número que você deseja somar ou 999 para encerrar o programa: '))
-    n = n + n
+    n1 = n
+    n2 = n + n1
     contador += 1
-print(f'\nVocê digitou {contador-1} numeros, e o somatório de todos eles deu {n-999}.')
+print(f'\nVocê digitou {contador-1} numeros, e o somatório de todos eles deu {n2-999}.')
