a411b1eef3db864b3b6f6c41bbd700a5ec884d2a
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-27 14:07:16 -0300

    Sequencia de estudos

--- Aulas Curso em Vídeo/Desafio 064 - Aula 14 - Estruturas de Repetição.py
+++ Aulas Curso em Vídeo/Desafio 064 - Aula 14 - Estruturas de Repetição.py
@@ -1,12 +1,8 @@
-print('Calculadora de Números Aleatórios')
+print('Calculadora de numeros aleatórios!')
+n = 0
 contador = 0
-while True:
-    if n != 999:
-        n = int(input('Digite seu número: '))
-        n1 = n
-        n2 = n1 + n
-        contador += 1
-        print(n2)
-    else:
-        print('Encerrando o programa!')
-        break
+while n != 999:
+    n = int(input('\nDigite o número que você deseja somar ou 999 para encerrar o programa: '))
+    n = n + n
+    contador += 1
+print(f'\nVocê digitou {contador-1} numeros, e o somatório de todos eles deu {n-999}.')
