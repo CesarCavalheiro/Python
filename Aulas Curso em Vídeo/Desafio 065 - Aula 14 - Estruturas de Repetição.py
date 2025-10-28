34d41dfafcd7ddba9a53e710b8b0633c784adec4
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-10-28 14:00:23 -0300

    Update Desafio 065 - Aula 14 - Estruturas de Repetição.py

---
+++ Aulas Curso em Vídeo/Desafio 065 - Aula 14 - Estruturas de Repetição.py
@@ -0,0 +1,11 @@
+print('Calculadora de números aleatórios!!')
+while True:
+    cond = str()
+    n = int(input('\nDigite o número que você gostaria de somar: '))
+    cond = 'Você gostaria de continuar? '
+    if cond == 'sim':
+        ntotal = int()
+        n = int(input('\nDigite o número que você gostaria de somar: '))
+        ntotal = n + ntotal
+        cond = 'Você gostaria de continuar? '
+    
