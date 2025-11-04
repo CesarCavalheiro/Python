1ec8a4c614ce6a5bfd15454fb4fb32a9f38f443b
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-11-04 14:02:34 -0300

    Avanço nos estudos.

--- Aulas Curso em Vídeo/Ex050- Aulas de Exercícios (Aula13).py
+++ Aulas Curso em Vídeo/Ex050- Aulas de Exercícios (Aula13).py
@@ -1,8 +1,8 @@
 total = 0
-contador = 1
+contador = 0
 for n in range(1,7):
     n = int(input(f'Digite seu {contador}º número: '))
-    contador += 1
-    if n % 2 != 0:
+    if n % 2 == 0:
+        contador += 1
         total = n + total
-print(f'\nA soma dos números ímpares digital é ?{total}')
+print(f'\nVocê digitou um total de {contador} números pares. A soma dos números pares digitados é: {total}')
---
+++ Aulas Curso em Vídeo/Ex051- Aulas de Exercícios (Aula13).py
@@ -0,0 +1,9 @@
+print('=' * 40)
+print('          10 TERMOS DE UMA PA')
+print('=' * 40)
+termo = int(input('\nDigite seu primeiro termo: '))
+razao = int(input('Digite a razão: '))
+pa = termo + (10 - 1) * razao
+for t in range(termo, pa, razao):
+    print(t, end=' --> ') 
+print('ACABOU')
