9c4fedbbbfe482492df762964f6da9f329b2e379
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-11-04 22:16:52 -0300

    Avanço nos estudos

--- Aulas Curso em Vídeo/Ex052- Aulas de Exercícios (Aula13).py
+++ Aulas Curso em Vídeo/Ex052- Aulas de Exercícios (Aula13).py
@@ -1,20 +1,41 @@
-n = int(input('Digite um número qualquer: '))
-contador = 0
+# verde = '\033[32m'
+# reset = '\033[0m'
 
-print()
+# n = int(input('Digite um número qualquer: '))
+# print()
 
-for i in range(1,n+1):
-    print(i, end=(' '))
+# contador = 0
 
-if n % i == 0:
-    contador +=1
+# for i in range(1, n +1):
+#     if n % i == 0:
+#         print(f'{verde}{i}{reset}', end=' ')
+#         contador += 1
+#     else:
+#         print(i, end=' ')
 
-if contador == 2:
-    print(f'''\nO número {n} foi divisível {contador} vezes!
-Por isso ele é primo!!!''')
+# print( )
 
-else:
-    print(f'\nO número {n} foi divisível {contador} vezes!')
-    print(f'\nPor isso ele não é PRIMO!!!')
+# if contador == 2:
+#     print(f'\nO número {n} foi divisível {contador} vezes!')
+#     print('Por isso ele é primo!!!')
+
+# else:
+#     print(f'\nO número {n} foi divisível {contador} vezes!')
+#     print(f'\nPor isso ele não é PRIMO!!!')
      
- 
+
+# ESTUDAR ESSE JEITO DE FAZER
+
+verde = '\032[034m'
+reset = '\033[0m'
+
+num = int(input('Digite seu número: '))
+
+for c in range(1, num, + 1):
+    if num % c == 0:
+        print('\033[034m', end= '')
+    else:
+        print ('\033[m', end='')
+    print(f'{c} ', end='')
+      
+
