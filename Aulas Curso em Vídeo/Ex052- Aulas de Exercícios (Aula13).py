7a3ea7d9d93f492442cc8c647aa7a368426d7cf1
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-11-04 21:41:31 -0300

    Update Ex052- Aulas de Exercícios (Aula13).py

--- Aulas Curso em Vídeo/Ex052- Aulas de Exercícios (Aula13).py
+++ Aulas Curso em Vídeo/Ex052- Aulas de Exercícios (Aula13).py
@@ -1,8 +1,20 @@
 n = int(input('Digite um número qualquer: '))
 contador = 0
+
+print()
+
 for i in range(1,n+1):
     print(i, end=(' '))
-    if n % i == 0:
-        contador += 1
-        print(f'O número foi divisível {contador} vezes!')
-    
+
+if n % i == 0:
+    contador +=1
+
+if contador == 2:
+    print(f'''\nO número {n} foi divisível {contador} vezes!
+Por isso ele é primo!!!''')
+
+else:
+    print(f'\nO número {n} foi divisível {contador} vezes!')
+    print(f'\nPor isso ele não é PRIMO!!!')
+     
+ 
