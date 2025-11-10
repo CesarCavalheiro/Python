fdeefa904688ac9de080de4b837f49e2986f9fe0
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-11-09 22:42:23 -0300

    Update Ex056- Aulas de Exercícios (Aula13).py

--- Aulas Curso em Vídeo/Ex056- Aulas de Exercícios (Aula13).py
+++ Aulas Curso em Vídeo/Ex056- Aulas de Exercícios (Aula13).py
@@ -3,7 +3,9 @@ idades = []
 sexos = []
 
 menoridade = 0
-
+sexof = 0
+anciao = ''
+ancião = ''
 
 for p in range(1, 4):
     print(f'----- {p}ª PESSOA -----')
@@ -15,27 +17,31 @@ for p in range(1, 4):
     idades.append(idade)
     sexos.append(sexo)
 
+    if sexo == 'F':
+        sexof += 1
+
     if sexo == 'F' and idade < 20:
         menoridade += 1
-    
-#print(f'\nA média de idade do grupo é {sum(idades)/3}')
-    
+
     if sexo == 'M' and idade == max(idades):
-        print(f'\nO homem mais velho do grupo tem {idade} e seu nome é {nome}!')
+        anciao = idade
+        ancião = nome
 
-#     else:
-#         print('\nNão há homens no grupo!')
 
-# if menoridade == 0:
-#     print('\nNão há mulheres com menos de 20 anos no grupo!')
+print(f'\nA média de idade do grupo é {sum(idades)/3:.2f} anos!')
+
+if sexof < 3:
+    print(f'\nO homem mais velho do grupo tem {anciao} anos e seu nome é {ancião}!')
+
+if sexof == 0:
+    print('\nNão há mulheres no grupo!')
+
+if sexof > 0 and menoridade == 0:
+    print('\nNão há mulheres com menos de 20 anos no grupo!')
     
-# elif menoridade < 2:
-       
-#     print(f'\nHá uma mulher com menos de 20 anos no grupo!')
+if sexof >= 1 and menoridade == 1:
+   print(f'\nHá uma mulher com menos de 20 anos no grupo!')
 
-# else:
-        
-#     print(f'\nAo todo são {menoridade} mulheres com menos de 20 anos no grupo!')
-
-    
+if sexof >= 1 and menoridade >= 2:
+    print(f'\nAo todo são {menoridade} mulheres com menos de 20 anos no grupo!')
 
