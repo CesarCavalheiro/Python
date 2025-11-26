3c90f31ac03bc37de6f66bd709ed13ef702c1cbd
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-11-26 20:10:47 -0300

    Update Desafio 084 - Aula 17 - Variáveis Compostas (Listas) - Listas dentro de Listas com Análise de Dados .py

--- Aulas Curso em Vídeo/Desafio 084 - Aula 17 - Variáveis Compostas (Listas) - Listas dentro de Listas com Análise de Dados .py
+++ Aulas Curso em Vídeo/Desafio 084 - Aula 17 - Variáveis Compostas (Listas) - Listas dentro de Listas com Análise de Dados .py
@@ -1,47 +1,27 @@
-lista = []
-menor_peso = ''
-
-
+lista = [ ]
+cond = 's'
 cont = 0
-condição = 's'
+menor_peso = 0
 
 while True:
-    if condição == 's':
-    
-        cont += 1
 
-        nome = str(input(f'\nNome: '))
-
+    if cond == 'S'.lower():
+        nome = str(input('\nNome: '))
         peso = float(input('\nPeso: '))
-
         lista.append([nome, peso])
+        
 
-        condição = str(input('\nQuer continuar [S/N]: ' ).lower())
+        for pessoa in lista:
+            if menor_peso <= peso:
+                print(pessoa)
+        
 
-    menor_peso == lista[0][1]
-
-    for pessoas in lista:
-        if pessoas[1] < menor_peso:
-            menor_peso = pessoas[1]
-
-    print(f'\nMenor peso encontrado foi: {peso} Kg!')
-
-    for pessoas in lista:
-        if p[1] == menor_peso:
-            print(f'\n - {p[0]}')
-
-       
-
-        condição = str(input('\nQuer continuar [S/N]: ' ).lower())
+        
+        
+        cond = str(input('\nDeseja continuar? [S/N]: '))
 
         
 
-            
-           
     else:
-
         print('\nVocê escolheu sair!!!')
         break
-
-print(f'Ao todo, você cadastrou {cont} pessoas.') 
- 
