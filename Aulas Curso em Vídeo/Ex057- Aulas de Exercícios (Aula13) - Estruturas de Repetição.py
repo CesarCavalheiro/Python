9cafb3c7dc5bd1642538ec2a71c17e7dfd010062
Cesar Cavalheiro Malaguez <cesar.cavalheiro@gmail.com>
2025-12-11 15:03:31 -0300

    Create Ex057- Aulas de Exercícios (Aula13) - Estruturas de Repetição.py

---
+++ Aulas Curso em Vídeo/Ex057- Aulas de Exercícios (Aula13) - Estruturas de Repetição.py
@@ -0,0 +1,11 @@
+while True:
+
+    sexo = str(input('\nInforme seu sexo: [M/F] ').lower())
+        
+    if sexo != 'm' or f:
+        print('\nDados Inválidos. Por favor, informe seu sexo com M ou F: ')
+
+    else:
+        print(f'\nSexo {sexo} registrado com sucesso!')
+    
+    
