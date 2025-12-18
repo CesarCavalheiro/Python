12492b92c36070cb15d337115c1f3e63eb8205eb
Cesar Cavalheiro <cesar.cavalheiro@gmail.com>
2025-12-18 08:15:59 -0300

    Update Ex070 - Aulas de Exercícios (Aula15) - Estruturas de Repetição (While True) - Preços de Produtos.py

--- Aulas Curso em Vídeo/Ex070 - Aulas de Exercícios (Aula15) - Estruturas de Repetição (While True) - Preços de Produtos.py
+++ Aulas Curso em Vídeo/Ex070 - Aulas de Exercícios (Aula15) - Estruturas de Repetição (While True) - Preços de Produtos.py
@@ -3,16 +3,40 @@ nome = 'LOJA SUPER BARATÃO'
 print(f'{nome:^60}')
 print('-' * 60)
 
+cont = 0
+lista = []
+lista1 = []
+total = 0
+nomebarato = ''
+valor = ''
+
 while True:
 
-    produto = str(input('Nome do Produto: '))
+    produto = str(input('\nNome do Produto: '))
 
-    preço = float(input('Preço: ').replace(',' , '.'))
+    preço = float(input('Preço: R$ ').replace(',' , '.'))
 
     ação = ''
-    
-    while ação not in ('s' , 'n'):
-        ação = str(input('Quer continuar? [S/N]: ')).lower().strip()[0]
-        if ação == 'n':
-            break
 
+    total = total + preço
+
+    if preço > 1000:
+        cont += 1
+    
+    
+    lista.append(preço)
+    min(lista)
+
+    if valor == min(lista):
+        nomebarato = produto
+    
+    while ação != 's' and ação != 'n':
+        ação = str(input('\nQuer continuar? [S/N]: ')).lower().strip()[0]
+
+    if ação == 'n':
+        break
+
+print('-' * 16, ' FIM DO PROGRAMA ' , '-'*16)
+print(f'O total da compra foi de R$ {total:.2f}')
+print(f'Temos {cont} produtos custando mais de R$ 1000,00')
+print(f'O produto mais barato foi {nomebarato} que custa R$ {min(lista):.2f}. ')
